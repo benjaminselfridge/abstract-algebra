b6baacd3359f35395ab5bf073cb3a50df3044dd3
Ben Selfridge <benselfridge@galois.com>
2025-02-25 12:59:04 -0800

    Add group actions and subgroup criterion

--- Lib/Defs.lean
+++ Lib/Defs.lean
@@ -1,27 +1,11 @@
 -- import Mathlib.Algebra.Group.Defs
 import Lib.One
 import Lib.Inv
+import Mathlib.Data.Set.Basic
 
 namespace AbstractAlgebra
 
 open Lib
-
-/- Minimal definition of a set -/
-def Set (α : Type u) := α → Prop
-
-/-- Turn a predicate `p : α → Prop` into a set, also written as `{x | p x}` -/
-def setOf {α : Type u} (p : α → Prop) : Set α :=
-  p
-
-namespace Set
-
-protected def Mem (s : Set α) (a : α) : Prop :=
-  s a
-
-instance : Membership α (Set α) :=
-  ⟨Set.Mem⟩
-
-end Set
 
 /- Minimal definition of a group -/
 
@@ -42,15 +26,15 @@ open Group
 
 namespace Group
 
+section
+
 variable {G : Type u} [Group G]
 variable (a b c : G)
 
 theorem mul_inv_cancel (a : G) : a * a⁻¹ = 1 := by
-  calc a * a⁻¹ = 1 * a * a⁻¹             := by rw [one_mul]
-       _       = (a⁻¹)⁻¹ * a⁻¹ * a * a⁻¹ := by rw [inv_mul_cancel]
-       _       = (a⁻¹)⁻¹ * 1 * a⁻¹       := by rw [mul_assoc (a⁻¹)⁻¹, inv_mul_cancel]
-       _       = (a⁻¹)⁻¹ * a⁻¹           := by rw [mul_assoc, one_mul]
-       _       = 1                       := by rw [inv_mul_cancel]
+  calc a * a⁻¹ = a⁻¹⁻¹ * a⁻¹ * a * a⁻¹ := by rw [inv_mul_cancel, one_mul]
+       _       = a⁻¹⁻¹ * a⁻¹           := by rw [mul_assoc a⁻¹⁻¹, inv_mul_cancel, mul_assoc, one_mul]
+       _       = 1                     := by rw [inv_mul_cancel]
 
 theorem mul_one (a : G) : a * 1 = a := by
   calc a * 1 = a * a⁻¹ * a := by rw [mul_assoc, inv_mul_cancel]
@@ -65,24 +49,20 @@ theorem id_unique : (∀ x : G, a * x = 1) → a = 1 := by
 
 theorem inv_unique : a * b = 1 → b = a⁻¹ := by
   intro h
-  calc b = 1 * b       := by rw [one_mul]
-       _ = a⁻¹ * a * b := by rw [inv_mul_cancel]
+  calc b = a⁻¹ * a * b := by rw [inv_mul_cancel, one_mul]
        _ = a⁻¹ * 1     := by rw [mul_assoc, h]
        _ = a⁻¹         := by rw [mul_one]
 
-theorem inv_inv : (a⁻¹)⁻¹ = a := by
-  calc (a⁻¹)⁻¹ = (a⁻¹)⁻¹ * 1       := by rw [mul_one]
-       _       = (a⁻¹)⁻¹ * a⁻¹ * a := by rw [mul_assoc, inv_mul_cancel]
-       _       = 1 * a             := by rw [inv_mul_cancel]
-       _       = a                 := by rw [one_mul]
+theorem inv_inv : a⁻¹⁻¹ = a := by
+  calc a⁻¹⁻¹ = a⁻¹⁻¹ * 1       := by rw [mul_one]
+       _     = a⁻¹⁻¹ * a⁻¹ * a := by rw [mul_assoc, inv_mul_cancel]
+       _     = 1 * a           := by rw [inv_mul_cancel]
+       _     = a               := by rw [one_mul]
 
 theorem mul_inv_rev : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
-  calc (a * b)⁻¹ = (a * b)⁻¹ * 1                 := by rw [mul_one]
-       _         = (a * b)⁻¹ * a * a⁻¹           := by rw [mul_assoc, mul_inv_cancel]
-       _         = (a * b)⁻¹ * a * 1 * a⁻¹       := by rw [mul_one]
-       _         = (a * b)⁻¹ * a * b * b⁻¹ * a⁻¹ := by rw [mul_assoc ((a * b)⁻¹ * a) b, mul_inv_cancel];
-       _         = 1 * b⁻¹ * a⁻¹                 := by rw [mul_assoc (a * b)⁻¹, inv_mul_cancel]
-       _         = b⁻¹ * a⁻¹                     := by rw [one_mul]
+  calc (a * b)⁻¹ = (a * b)⁻¹ * a * a⁻¹           := by rw [mul_assoc, mul_inv_cancel, mul_one]
+       _         = (a * b)⁻¹ * a * b * b⁻¹ * a⁻¹ := by rw [mul_assoc ((a * b)⁻¹ * a) b, mul_inv_cancel, mul_one];
+       _         = b⁻¹ * a⁻¹                     := by rw [mul_assoc (a*b)⁻¹, inv_mul_cancel, one_mul]
 
 -- Proposition 1.1.5 could sort of be demonstrated by doing a tactic.
 
@@ -97,24 +77,46 @@ theorem mul_right_cancel : a * c = b * c → a = b := by
   calc a = a * c * c⁻¹ := by rw [mul_assoc, mul_inv_cancel, mul_one]
        _ = b * c * c⁻¹ := by rw [h]
        _ = b           := by rw [mul_assoc, mul_inv_cancel, mul_one]
-
+end
 end Group
 
 variable {G H : Type u} [Group G] [Group H]
-variable (a b c : G)
-variable (x y z : H)
 
-/-
-instance : Group ℤ where
-  inv x := -x
-  mul x y := x + y
-  one := 0
-
-  mul_assoc a b c := Int.add_assoc a b c
-  one_mul a := Int.zero_add a
-  inv_mul_cancel a := Int.add_left_neg a
-
-end AbstractAlgebra
--/
+variable {A B : Set α}
 
 def IsHom (φ : G → H) : Prop :=  ∀ a b : G, φ (a * b) = φ a * φ b
+
+structure GroupAction (G:Type*) (A : Set G) [Group G] where
+  apply : G → A → A
+  one_action (a : A) : apply 1 a = a
+  action_assoc (g₁ g₂ : G) (a : A) : apply g₁ (apply g₂ a) = apply (g₁ * g₂) a
+
+structure Subgroup (G:Type*) [Group G] where
+  carrier : Set G
+  mul_mem : ∀ a b, a ∈ carrier → b ∈ carrier → a * b ∈ carrier
+  one_mem : 1 ∈ carrier
+  inv_mem : ∀ a, a ∈ carrier → a⁻¹ ∈ carrier
+
+def subgroup_criterion {G} [Group G] (H : Set G) : Prop :=
+  (1 ∈ H) ∧
+  (∀ h₁ h₂ : G, h₁ ∈ H ∧ h₂ ∈ H → h₁⁻¹ * h₂ ∈ H)
+
+def Subgroup_of_subgroup_criterion {G} [Group G] (H : Set G)
+   (sc : subgroup_criterion H) : Subgroup G :=
+   have inv_mem : ∀ a : G, a ∈ H → a⁻¹ ∈ H := by
+     intro a h
+     rw [← mul_one a⁻¹]
+     apply sc.right
+     exact ⟨h, sc.left⟩
+   Subgroup.mk
+    H
+    (by intros a b
+        show a ∈ H → b ∈ H → a * b ∈ H
+        intros hp₁ hp₂
+        have : a * b = a⁻¹⁻¹ * b := by rw [inv_inv]
+        rw [this]
+        apply sc.right
+        exact ⟨inv_mem a hp₁, hp₂⟩
+    )
+    sc.left
+    inv_mem
