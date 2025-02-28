f8cbca91cf06487ebb17f51b2aaf488d2eb3c93c
Benjamin Selfridge <benselfridge@galois.com>
2025-02-28 09:07:11 -0800

    Add proof that nonzero rationals form a multiplicative group

--- AbstractAlgebra/Ch1/Sec1.lean
+++ AbstractAlgebra/Ch1/Sec1.lean
@@ -1,6 +1,5 @@
 import AbstractAlgebra.One
 import AbstractAlgebra.Inv
-
 import Mathlib.Data.Complex.Basic
 import Mathlib.Data.Rat.Defs
 import Mathlib.Data.Rat.Lemmas
@@ -72,13 +71,40 @@ structure ℚnz where
 #check ℚnz.elt_is_nonzero
 
 example : (1:ℚ) ≠ 0 := by apply?
+#check inv_ne_zero
 
-instance: Group ℚnz where
+theorem eq_ℚnz (a b : ℚnz) : a = b ↔ a.elt = b.elt := by
+  constructor
+  . intro h; rw [h]
+  . intro h
+    calc a = ⟨a.elt, a.elt_is_nonzero⟩ := by rfl
+         _ = ⟨b.elt, b.elt_is_nonzero⟩ := by simp [h]
+         _ = b                         := by rfl
+
+instance: Mul ℚnz where
   mul x y :=
     ℚnz.mk (x.elt * y.elt)
       (by rw [mul_ne_zero_iff_right] <;> apply ℚnz.elt_is_nonzero)
-  one := ℚnz.mk 1 (Ne.symm Rat.zero_ne_one)
-  inv := sorry
-  mul_assoc := sorry
-  one_mul := sorry
-  inv_mul_cancel := sorry
+
+instance: One ℚnz where
+  one := ⟨1, Ne.symm Rat.zero_ne_one⟩
+
+instance: Inv ℚnz where
+  inv x := ℚnz.mk x.elt⁻¹
+    (by apply inv_ne_zero; exact x.elt_is_nonzero)
+
+instance: Group ℚnz where
+  mul_assoc a b c := by
+    rw [eq_ℚnz]
+    calc  (a * b * c).elt = (a * b).elt * c.elt := by rfl
+          _               = a.elt * b.elt * c.elt := by rfl
+          _               = a.elt * (b.elt * c.elt) := by apply Rat.mul_assoc
+  one_mul a := by
+    rw [eq_ℚnz]
+    calc  (1 * a).elt = 1 * a.elt := by rfl
+          _           = a.elt := Rat.one_mul a.elt
+  inv_mul_cancel a := by
+    rw [eq_ℚnz]
+    calc  (a⁻¹ * a).elt = a⁻¹.elt * a.elt := by rfl
+          _             = (a.elt)⁻¹ * a.elt := by rfl
+          _             = 1 := Rat.inv_mul_cancel a.elt a.elt_is_nonzero
